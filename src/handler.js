5a60e73f65b24dcd4b9239424ec2bb6fe20efce0
husnulnawafil <husnulnawafil27@gmail.com>
2022-11-18 08:52:31 +0700

    add update note route

--- src/handler.js
+++ src/handler.js
@@ -67,4 +67,42 @@ const getNoteByIdHandler = (req, res) => {
   return response;
 };
 
-module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler };
+const editNoteByIdHandler = (req, res) => {
+  const { id } = req.params;
+
+  const { title, tags, body } = req.payload;
+  const updatedAt = new Date().toISOString();
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes[index] = {
+      ...notes[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+
+    const response = res.response({
+      status: 'success',
+      message: 'Catatan berhasil diperbaharui',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = res.response({
+    status: 'fail',
+    message: 'Gagal memperbaharui. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+};
